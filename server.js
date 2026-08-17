1735a5bf1672af88a44d687620121b27e81249ef
dhif007 <mrizkidhifan@gmail.com>
2026-08-17 12:00:16 +0700

    Serve QR code browser library

--- server.js
+++ server.js
@@ -15,6 +15,12 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use(
+  "/vendor/qrcode",
+  express.static(
+    path.join(__dirname, "node_modules", "qrcode", "build")
+  )
+);
 /*
  * =========================================================
  * DATABASE
